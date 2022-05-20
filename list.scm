d2e9b03bfda5ec63492e0ae90b3e70c29d4a9ff3
wsingB <lee.wyt.lly@gmail.com>
2022-05-20 20:06:01 +0800

    list sets

--- list.scm
+++ list.scm
@@ -1,5 +1,18 @@
-(define map
-  (lambda(proc items)
+;; list operands sets
+
+
+(define (list-ref items n)
+  (if (= n 0)
+      (car items)
+      (list-ref (cdr items) (-1+ n))))
+
+(define length
+  (lambda(items)
     (if (null? items)
-        nil
-        (cons (map proc (proc items))))))
+        0
+        (+ 1 (length (cdr items))))))
+
+(define (length-iter items count)
+  (if (null? items)
+      count
+      (length-iter (cdr items) (+ count 1))))
