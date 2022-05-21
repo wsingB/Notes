1454fdcf0169ecc253c411f2054f822ee6a9cd49
wsingB <lee.wyt.lly@gmail.com>
2022-05-21 20:28:16 +0800

    update  insert sort

---
+++ algorithm/sort/insert_sort.c
@@ -0,0 +1,12 @@
+#include "./common.h"
+
+/*
+ * for insert_sort, it will be devidide into two parts;
+ * sorted, unsorted
+*/
+
+void
+insert_sort (void *vec, size_t type_size, int (*compare) (void *, void *))
+{
+
+}
