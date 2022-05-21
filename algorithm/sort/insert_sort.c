b00b8b761402eca537efd5162944672c2fb149c5
wsingB <lee.wyt.lly@gmail.com>
2022-05-21 20:37:55 +0800

    Generic sort

--- algorithm/sort/insert_sort.c
+++ algorithm/sort/insert_sort.c
@@ -4,9 +4,18 @@
  * for insert_sort, it will be devidide into two parts;
  * sorted, unsorted
 */
+static void copy_void_data (void *src, void *dst, size_t type_size);
 
 void
-insert_sort (void *vec, size_t type_size, int (*compare) (void *, void *))
+insert_sort (void *vec, size_t type_size, size_t n,  int (*compare) (void *, void *))
 {
-
+    for (int i = 1; i < n; ++i) {
+        void *key = vec + type_size * i;
+        int j = i - 1;
+        while (j > 0 && compare (vec + (type_size * j), key)) {
+            copy_void_data (vec + j *type_size, vec + (j + 1) * type_size, type_size);
+            j = j - 1;
+        }
+        copy_void_data(vec + (j + 1) * type_size, key);
+    }
 }
