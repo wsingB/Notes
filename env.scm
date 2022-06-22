99a67a970031d32c26c5e9a3a3d0223c3bc3f89f
wsingB <lee.wyt.lly@gmail.com>
2022-06-23 04:52:46 +0800

    OK EDA

--- env.scm
+++ env.scm
@@ -1,13 +1,6 @@
-(define square
-  (lambda(x) (* x x)))
+#include <stdio.h>
 
-(define sum-fo-squares
-  (lambda(x y)
-    (+ (square x)
-       (square y))))
-;; withdraw
-(define (make-withdraw balance)
-  (lambda(amount)
-    (if (>= balance amount)
-        (begin (set! balance (- balance amount)) balance) ;; begin order apply value.
-        "Insufficient funds")))
+int main () {
+  int ac;
+  printf("");
+}
