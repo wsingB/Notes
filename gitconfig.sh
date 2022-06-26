37d46fc3e75fa878eb5939d58c91636e1f11a6ac
wsingB <lee.wyt.lly@gmail.com>
2022-06-26 07:24:21 +0000

    Prepare to be master

--- gitconfig.sh
+++ gitconfig.sh
@@ -1,4 +1,4 @@
 git config --global user.name "wsingB"
 git config --global user.email "lee.wyt.lly@gmail.com"
-git config --global core.editor emacs
+git config --global core.editor vim
 git config --global color.ui true
